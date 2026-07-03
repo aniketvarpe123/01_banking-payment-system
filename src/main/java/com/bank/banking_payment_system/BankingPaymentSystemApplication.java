a425eae074cfe36a4122fdda9a50a47b32856fe9
Aniket Babasaheb Varpe <110764271+aniketvarpe123@users.noreply.github.com>
2026-07-03 15:15:55 +0530

    Initial commit

---
+++ src/main/java/com/bank/banking_payment_system/BankingPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bank.banking_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingPaymentSystemApplication.class, args);
+	}
+
+}
